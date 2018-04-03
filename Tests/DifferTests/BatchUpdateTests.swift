25ca20582259bcdcc160ab4435154cbade627fef
Tony Arnold <tony@thecocoabots.com>
2018-04-03 10:13:31 +1000

    Attempt to fix batch update tests (not working)

--- Tests/DifferTests/BatchUpdateTests.swift
+++ Tests/DifferTests/BatchUpdateTests.swift
@@ -1,67 +1,59 @@
 #if !os(macOS) && !os(watchOS)
-
-    import XCTest
     @testable import Differ
+    import XCTest
 
-    func IP(_ row: Int, _ section: Int) -> IndexPath {
+    private func IP(_ row: Int, _ section: Int) -> IndexPath {
         return IndexPath(row: row, section: section)
     }
 
     class BatchUpdateTests: XCTestCase {
+        private struct Expectation {
+            let orderBefore: [Int]
+            let orderAfter: [Int]
+            let insertions: [IndexPath]
+            let deletions: [IndexPath]
+            let moves: [(from: IndexPath, to: IndexPath)]
+        }
 
-        let cellExpectations: [([Int], [Int], ([IndexPath], [IndexPath], [(from: IndexPath, to: IndexPath)]))] = [
-            ([1, 2, 3, 4], [1, 2, 3, 4], ([], [], [])),
-            ([1, 2, 3, 4], [4, 2, 3, 1], ([], [], [(IP(0, 0), IP(3, 0)), (IP(3, 0), IP(0, 0))])),
-            ([1, 2, 3, 4], [2, 3, 1], ([IP(3, 0)], [], [(IP(0, 0), IP(2, 0))])),
-            ([1, 2, 3, 4], [5, 2, 3, 4], ([IP(0, 0)], [IP(0, 0)], [])),
-            ([1, 2, 3, 4], [4, 1, 3, 5], ([IP(1, 0)], [IP(3, 0)], [(IP(2, 0), IP(0, 0))])),
-            ([1, 2, 3, 4], [4, 2, 3, 4], ([IP(0, 0)], [IP(0, 0)], [])),
-            ([1, 2, 3, 4], [1, 2, 4, 4], ([IP(2, 0)], [IP(3, 0)], [])),
-            ([1, 2, 3, 4], [5, 6, 7, 8], ([IP(0, 0), IP(1, 0), IP(2, 0), IP(3, 0)], [IP(0, 0), IP(1, 0), IP(2, 0), IP(3, 0)], [])),
-            ([1, 2, 3, 4], [5, 6, 7, 1], ([IP(1, 0), IP(2, 0), IP(3, 0)], [IP(0, 0), IP(1, 0), IP(2, 0)], []))
+        private let cellExpectations: [Expectation] = [
+            Expectation(orderBefore: [1, 2, 3, 4], orderAfter: [1, 2, 3, 4], insertions: [], deletions: [], moves: []),
+            Expectation(orderBefore: [1, 2, 3, 4], orderAfter: [4, 2, 3, 1], insertions: [], deletions: [], moves: [(IP(0, 0), IP(3, 0)), (IP(3, 0), IP(0, 0))]),
+            Expectation(orderBefore: [1, 2, 3, 4], orderAfter: [2, 3, 1], insertions: [IP(3, 0)], deletions: [], moves: [(IP(0, 0), IP(2, 0))]),
+            Expectation(orderBefore: [1, 2, 3, 4], orderAfter: [5, 2, 3, 4], insertions: [IP(0, 0)], deletions: [IP(0, 0)], moves: []),
+            Expectation(orderBefore: [1, 2, 3, 4], orderAfter: [4, 1, 3, 5], insertions: [IP(1, 0)], deletions: [IP(3, 0)], moves: [(IP(2, 0), IP(0, 0))]),
+            Expectation(orderBefore: [1, 2, 3, 4], orderAfter: [4, 2, 3, 4], insertions: [IP(0, 0)], deletions: [IP(0, 0)], moves: []),
+            Expectation(orderBefore: [1, 2, 3, 4], orderAfter: [1, 2, 4, 4], insertions: [IP(2, 0)], deletions: [IP(3, 0)], moves: []),
+            Expectation(orderBefore: [1, 2, 3, 4], orderAfter: [5, 6, 7, 8], insertions: [IP(0, 0), IP(1, 0), IP(2, 0), IP(3, 0)], deletions: [IP(0, 0), IP(1, 0), IP(2, 0), IP(3, 0)], moves: []),
+            Expectation(orderBefore: [1, 2, 3, 4], orderAfter: [5, 6, 7, 1], insertions: [IP(1, 0), IP(2, 0), IP(3, 0)], deletions: [IP(0, 0), IP(1, 0), IP(2, 0)], moves: [])
         ]
 
         func testCells() {
-            _testCells()
+            self._testCells()
         }
 
         func testCellsWithTransform() {
-            _testCellsWithTransform()
+            self._testCellsWithTransform()
         }
 
         func _testCells() {
-            for expectation in cellExpectations {
-                let batch = BatchUpdate(diff: expectation.0.extendedDiff(expectation.1))
-                XCTAssertEqual(batch.deletions, expectation.2.0)
-                XCTAssertEqual(batch.insertions, expectation.2.1)
-                XCTAssertEqual(batch.moves, expectation.2.2)
+            for expectation in self.cellExpectations {
+                let batch = BatchUpdate(diff: expectation.orderBefore.extendedDiff(expectation.orderAfter))
+                XCTAssertEqual(batch.deletions, expectation.deletions)
+                XCTAssertEqual(batch.insertions, expectation.insertions)
+                XCTAssertEqual(batch.moves, expectation.moves)
             }
         }
 
         func _testCellsWithTransform() {
             let transform: (IndexPath) -> IndexPath = { IP($0.row + 1, $0.section + 2) }
 
-            for expectation in cellExpectations {
-                let batch = BatchUpdate(diff: expectation.0.extendedDiff(expectation.1), indexPathTransform: transform)
-                XCTAssertEqual(batch.deletions, expectation.2.0.map(transform))
-                XCTAssertEqual(batch.insertions, expectation.2.1.map(transform))
-                XCTAssertEqual(batch.moves, expectation.2.2.map { (transform($0.0), transform($0.1)) })
+            for expectation in self.cellExpectations {
+                let batch = BatchUpdate(diff: expectation.orderBefore.extendedDiff(expectation.orderAfter), indexPathTransform: transform)
+                XCTAssertEqual(batch.deletions, expectation.deletions.map(transform))
+                XCTAssertEqual(batch.insertions, expectation.insertions.map(transform))
+                XCTAssertEqual(batch.moves, expectation.moves.map { (transform($0.0), transform($0.1)) })
             }
         }
-
-        //
-        //    // MARK: Performance Tests
-        //    func testCellsPerformance() {
-        //        self.measure {
-        //            self._testCells()
-        //        }
-        //    }
-        //
-        //    func testCellsWithTransformPerformance() {
-        //        self.measure {
-        //            self._testCellsWithTransform()
-        //        }
-        //    }
     }
 
 #endif
